74c68036811077c6e2d905fd050c45704b160c5e
Ibrahim Santana Velazquez <315158477+ibrahimsantana@users.noreply.github.com>
2026-09-05 15:06:06 -0600

    feat(remotion): extend Bellas Artes V2 to preserve full Jen message

--- remotion/bellas-artes-2026/src/Root.tsx
+++ remotion/bellas-artes-2026/src/Root.tsx
@@ -8,7 +8,7 @@ export const Root: React.FC = () => {
       <Composition
         id="PMX-BellasArtes-Reel-Master"
         component={BellasArtesReel}
-        durationInFrames={810}
+        durationInFrames={1210}
         fps={30}
         width={1080}
         height={1920}
