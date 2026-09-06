89d8f5e1d896950a451d337762cc44c21f08e5b0
Ibrahim Santana Velazquez <315158477+ibrahimsantana@users.noreply.github.com>
2026-09-06 09:55:47 -0600

    feat: add 2026 theme and uniform calendar metadata

--- calendario/config.js
+++ calendario/config.js
@@ -18,21 +18,21 @@ window.PMX_SEASON_2026 = Object.freeze({
   games: Object.freeze([
     Object.freeze({ week: 1, dateISO: "2026-09-13", dateLabel: "DOM 13 SEP", time: "14:25", opponent: "Minnesota Vikings", short: "VIKINGS", site: "AWAY", tv: "CBS", prime: false, special: "SEASON OPENER" }),
     Object.freeze({ week: 2, dateISO: "2026-09-20", dateLabel: "DOM 20 SEP", time: "11:00", opponent: "New York Jets", short: "JETS", site: "AWAY", tv: "FOX", prime: false }),
-    Object.freeze({ week: 3, dateISO: "2026-09-24", dateLabel: "JUE 24 SEP", time: "18:15", opponent: "Atlanta Falcons", short: "FALCONS", site: "HOME", tv: "PRIME VIDEO", prime: true, special: "HOME OPENER" }),
+    Object.freeze({ week: 3, dateISO: "2026-09-24", dateLabel: "JUE 24 SEP", time: "18:15", opponent: "Atlanta Falcons", short: "FALCONS", site: "HOME", tv: "PRIME VIDEO", prime: true, special: "HOME OPENER · ALUMNI WEEKEND" }),
     Object.freeze({ week: 4, dateISO: "2026-10-04", dateLabel: "DOM 04 OCT", time: "11:00", opponent: "Tampa Bay Buccaneers", short: "BUCCANEERS", site: "AWAY", tv: "FOX", prime: false }),
-    Object.freeze({ week: 5, dateISO: "2026-10-11", dateLabel: "DOM 11 OCT", time: "14:25", opponent: "Chicago Bears", short: "BEARS", site: "HOME", tv: "FOX", prime: false, special: "RIVALRIES" }),
-    Object.freeze({ week: 6, dateISO: "2026-10-18", dateLabel: "DOM 18 OCT", time: "18:20", opponent: "Dallas Cowboys", short: "COWBOYS", site: "HOME", tv: "NBC", prime: true, special: "SNF" }),
+    Object.freeze({ week: 5, dateISO: "2026-10-11", dateLabel: "DOM 11 OCT", time: "14:25", opponent: "Chicago Bears", short: "BEARS", site: "HOME", tv: "FOX", prime: false, special: "NFL RIVALRIES", uniform: "PACKERS RIVALRIES · CONFIRMADO" }),
+    Object.freeze({ week: 6, dateISO: "2026-10-18", dateLabel: "DOM 18 OCT", time: "18:20", opponent: "Dallas Cowboys", short: "COWBOYS", site: "HOME", tv: "NBC", prime: true, special: "PACKERS VS. CANCER" }),
     Object.freeze({ week: 7, dateISO: "2026-10-25", dateLabel: "DOM 25 OCT", time: "14:25", opponent: "Detroit Lions", short: "LIONS", site: "AWAY", tv: "FOX", prime: false }),
-    Object.freeze({ week: 8, dateISO: "2026-10-29", dateLabel: "JUE 29 OCT", time: "18:15", opponent: "Carolina Panthers", short: "PANTHERS", site: "HOME", tv: "PRIME VIDEO", prime: true, special: "TNF" }),
+    Object.freeze({ week: 8, dateISO: "2026-10-29", dateLabel: "JUE 29 OCT", time: "18:15", opponent: "Carolina Panthers", short: "PANTHERS", site: "HOME", tv: "PRIME VIDEO", prime: true, special: "BOB HARLAN TRIBUTE" }),
     Object.freeze({ week: 9, dateISO: "2026-11-08", dateLabel: "DOM 08 NOV", time: "15:25", opponent: "New England Patriots", short: "PATRIOTS", site: "AWAY", tv: "FOX", prime: false }),
-    Object.freeze({ week: 10, dateISO: "2026-11-15", dateLabel: "DOM 15 NOV", time: "12:00", opponent: "Minnesota Vikings", short: "VIKINGS", site: "HOME", tv: "FOX", prime: false }),
+    Object.freeze({ week: 10, dateISO: "2026-11-15", dateLabel: "DOM 15 NOV", time: "12:00", opponent: "Minnesota Vikings", short: "VIKINGS", site: "HOME", tv: "FOX", prime: false, special: "SALUTE TO SERVICE" }),
     Object.freeze({ week: 11, dateISO: "2026-11-22", dateLabel: "WEEK 11", time: "", opponent: "BYE WEEK", short: "BYE", site: "BYE", tv: "", prime: false }),
     Object.freeze({ week: 12, dateISO: "2026-11-25", dateLabel: "MIÉ 25 NOV", time: "19:00", opponent: "Los Angeles Rams", short: "RAMS", site: "AWAY", tv: "NETFLIX", prime: true, special: "THANKSGIVING EVE" }),
     Object.freeze({ week: 13, dateISO: "2026-12-06", dateLabel: "DOM 06 DIC", time: "12:00", opponent: "New Orleans Saints", short: "SAINTS", site: "AWAY", tv: "FOX", prime: false }),
-    Object.freeze({ week: 14, dateISO: "2026-12-13", dateLabel: "DOM 13 DIC", time: "19:20", opponent: "Buffalo Bills", short: "BILLS", site: "HOME", tv: "NBC", prime: true, special: "SNF" }),
-    Object.freeze({ week: 15, dateISO: "2026-12-20", dateLabel: "DOM 20 DIC", time: "12:00", opponent: "Miami Dolphins", short: "DOLPHINS", site: "HOME", tv: "FOX", prime: false }),
-    Object.freeze({ week: 16, dateISO: "2026-12-25", dateLabel: "VIE 25 DIC", time: "12:00", opponent: "Chicago Bears", short: "BEARS", site: "AWAY", tv: "NETFLIX", prime: false, special: "NAVIDAD" }),
-    Object.freeze({ week: 17, dateISO: "2027-01-04", dateLabel: "LUN 04 ENE", time: "19:15", opponent: "Houston Texans", short: "TEXANS", site: "HOME", tv: "ESPN", prime: true, special: "MNF" }),
-    Object.freeze({ week: 18, dateISO: "", dateLabel: "SÁB 09 / DOM 10 ENE", time: "", opponent: "Detroit Lions", short: "LIONS", site: "HOME", tv: "TBD", prime: false, special: "DÍA / HORA TBD" })
+    Object.freeze({ week: 14, dateISO: "2026-12-13", dateLabel: "DOM 13 DIC", time: "19:20", opponent: "Buffalo Bills", short: "BILLS", site: "HOME", tv: "NBC", prime: true, special: "1923 CLASSIC · WALTER PAYTON MOY", uniform: "PACKERS 1923 CLASSIC · CONFIRMADO" }),
+    Object.freeze({ week: 15, dateISO: "2026-12-20", dateLabel: "DOM 20 DIC", time: "12:00", opponent: "Miami Dolphins", short: "DOLPHINS", site: "HOME", tv: "FOX", prime: false, special: "INSPIRE CHANGE" }),
+    Object.freeze({ week: 16, dateISO: "2026-12-25", dateLabel: "VIE 25 DIC", time: "12:00", opponent: "Chicago Bears", short: "BEARS", site: "AWAY", tv: "NETFLIX", prime: false, special: "CHRISTMAS DAY", opponentUniform: "BEARS RIVALRIES · CONFIRMADO" }),
+    Object.freeze({ week: 17, dateISO: "2027-01-04", dateLabel: "LUN 04 ENE", time: "19:15", opponent: "Houston Texans", short: "TEXANS", site: "HOME", tv: "ESPN", prime: true, special: "NFL PLAY 60" }),
+    Object.freeze({ week: 18, dateISO: "", dateLabel: "SÁB 09 / DOM 10 ENE", time: "", opponent: "Detroit Lions", short: "LIONS", site: "HOME", tv: "TBD", prime: false, special: "FAN APPRECIATION · DÍA / HORA TBD" })
   ])
 });
